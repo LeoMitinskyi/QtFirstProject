3507a518932463deba4433c2ee2a79a400e76598
LeoMitinskyi <73614899+LeoMitinskyi@users.noreply.github.com>
2022-07-15 18:46:18 +0300

    Add files via upload

---
+++ EighthProject/form.h
@@ -0,0 +1,29 @@
+#ifndef FORM_H
+#define FORM_H
+
+#include <QWidget>
+#include <QVector>
+
+namespace Ui {
+class Form;
+}
+
+class Form : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Form(QWidget *parent = nullptr);
+    ~Form();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Form *ui;
+
+signals:
+    void signal(QVector<QString>);
+};
+
+#endif // FORM_H
